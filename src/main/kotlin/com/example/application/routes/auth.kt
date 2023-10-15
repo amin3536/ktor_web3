a6e68d1fc0f4d797458b120a8b28fe2b638cdb60
Amin <am.a9173@gmail.com>
2023-10-15 14:47:23 +0330

    config jwt

--- src/main/kotlin/com/example/application/routes/auth.kt
+++ src/main/kotlin/com/example/application/routes/auth.kt
@@ -1,6 +1,7 @@
 package com.example.application.routes
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.example.domain.entity.User
 import com.example.domain.service.UserService
 import io.ktor.http.*
@@ -9,6 +10,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
+import java.util.*
 
 fun  Route.auth() {
     val service by inject<UserService>()
@@ -17,10 +19,20 @@ fun  Route.auth() {
         val id=service.createUser(user)
         call.respond(HttpStatusCode.Created,id)
     }
+    val secret =    environment?.config?.property("jwt.secret")?.getString()
+    val issuer =    environment?.config?.property("jwt.issuer")?.getString()
+    val audience =  environment?.config?.property("jwt.audience")?.getString()
+    val myRealm =   environment?.config?.property("jwt.realm")?.getString()
     post("/login") {
         val user=call.receive<User>()
         if (service.verifyUser(user)){
-
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("mail", user.mail)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(secret))
+            call.respond(hashMapOf("token" to token))
         }
     }
 }
--- src/main/kotlin/com/example/infrastructure/plugins/Security.kt
+++ src/main/kotlin/com/example/infrastructure/plugins/Security.kt
@@ -2,6 +2,7 @@ package com.example.infrastructure.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -9,33 +10,13 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureSecurity() {
-    authentication {
-        basic(name = "myauth1") {
-            realm = "Ktor Server"
-            validate { credentials ->
-                if (credentials.name == credentials.password) {
-                    UserIdPrincipal(credentials.name)
-                } else {
-                    null
-                }
-            }
-        }
-        form(name = "myauth2") {
-            userParamName = "user"
-            passwordParamName = "password"
-            challenge {
-                /**/
-            }
-
-        }
-    }
     // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
+    val jwtSecret = environment.config.property("jwt.secret").getString()
+    val jwtDomain = environment.config.property("jwt.issuer").getString()
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwtRealm = environment.config.property("jwt.realm").getString()
     authentication {
-        jwt {
+        jwt("auth-jwt") {
             realm = jwtRealm
             verifier(
                 JWT
@@ -47,19 +28,18 @@ fun Application.configureSecurity() {
             validate { credential ->
                 if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
             }
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
         }
     }
     routing {
-        authenticate("myauth1") {
-            get("/protected/route/basic") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
-            }
-        }
-        authenticate("myauth2") {
-            get("/protected/route/form") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
+        authenticate("auth-jwt") {
+            get("/protected/route/jwt") {
+                val principal = call.principal<JWTPrincipal>()!!
+                val mail = principal.payload.getClaim(",mail").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello ${mail}, your token expire at $expiresAt")
             }
         }
     }
